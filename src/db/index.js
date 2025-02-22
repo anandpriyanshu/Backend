6df151ba328f92efbe8b10de80d0b637c2063964
Priyanshu Anand <anandpriyanshu12@gmail.com>
2025-02-22 20:23:20 +0530

    connect to mongodb atlas

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,14 @@
+
+
 import mongoose from 'mongoose';
-import { DB_NAME } from '../constants';
+import { DB_NAME } from '../constants.js';
 
 const connectDB = async () => {
   try {
     const connectInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
     console.log(`\n MongoDB connect! DB HOST: ${connectInstance.connection.host}`)
   } catch (error) {
-    console.log("MONGODB Connection Error", error)
+    console.log("MONGODB Connection Failed", error)
     process.exit(1)
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-import mongoose from 'mongoose';
-import { DB_NAME } from './constants';
+// require('dotenv').config({ path: './env' })
+
+import dotenv from 'dotenv'
+import connectDB from './db/index.js';
 // import express from 'express'
+
+
+
+dotenv.config({
+  path: './env'
+})
+
+
+
+connectDB()
+
+
 
 // const app = express()
 //   (async () => {
