bbc9d7f55d6673002f876be91b8541fb62780c68
Priyanshu Anand <anandpriyanshu12@gmail.com>
2025-02-24 09:41:49 +0530

    Set configration in App.js & creat AsyncHandler function

--- src/index.js
+++ src/index.js
@@ -1,36 +1,22 @@
-// require('dotenv').config({ path: './env' })
 
 import dotenv from 'dotenv'
 import connectDB from './db/index.js';
-// import express from 'express'
-
+import { app } from './app.js';
 
 
 dotenv.config({
   path: './env'
 })
 
-
-
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running  at PORT : ${process.env.PORT}`)
+    })
+  })
+  .catch((error) => {
+    console.log("MONDO DB Connection failed", error)
+  })
 
 
 
-// const app = express()
-//   (async () => {
-
-//     try {
-//       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//       app.on('error', (error) => {
-//         console.log('Err', error)
-//         throw error
-//       })
-
-//       app.listen(process.env.PORT, () => {
-//         console.log(`APP is running on ${process.env.PORT}`)
-//       })
-//     } catch (error) {
-//       console.log(error.message)
-//       throw error
-//     }
-//   })()
